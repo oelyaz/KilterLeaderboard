e739f3b4434bb1308535a99e6885fe2385edeadd
oelyaz <oelyaz.seytl@gmail.com>
2025-04-03 13:16:59 +0200

    added logo, and made navbar adjustments

--- front/src/lib/Navbar.svelte
+++ front/src/lib/Navbar.svelte
@@ -8,10 +8,21 @@
     function navigate_register() {
         goto("/register")
     }
+
+    let logoSrc = '/logo.png'; // Path to the logo in the static folder
 </script>
 
-<nav>
-    <h1>Klieterboard</h1>
-    <button on:click={navigate_leaderboard}>Leaderboard</button>
-    <button on:click={navigate_register}>Register</button>
+<nav class="navbar">
+    <div class="logo">
+        <img src="{logoSrc}" alt="logo">
+    </div>
+    <h1 class="heading">KilterLeaderBoard</h1>
+    <div class="buttons">
+        <button on:click={navigate_leaderboard}>
+            <img src="node_modules/lucide-static/icons/medal.svg" alt="Leaderboard">
+        </button>
+        <button on:click={navigate_register}>
+            <img src="node_modules/lucide-static/icons/circle-plus.svg" alt="Register">
+        </button>
+    </div>
 </nav>
