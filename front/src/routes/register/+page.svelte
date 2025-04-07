763e35325d27a9cdaf33a47e97a6f6357d82321a
oelyaz <oelyaz.seytl@gmail.com>
2025-04-07 17:29:17 +0200

    registration page elements added

--- front/src/routes/register/+page.svelte
+++ front/src/routes/register/+page.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
     import Navbar from "$lib/Navbar.svelte"
     import "$lib/global.css"
+    import "./page.css"
+
+    let username: string="";
 </script>
 
 <Navbar />
 
-<body>If you want to join the board, you have to be a friend of somebody already on it.</body>
-<h2>Enter username to register:</h2>
+<div class="register">
+        <label class="input-row">Enter username to register:
+            <input
+                id="username"
+                type="text"
+                bind:value={username}
+                placeholder=""
+            />
+        </label>
+    <i class="friend_disc">If you want to join the board, you have to be a friend of somebody already on it.</i>
+</div>
