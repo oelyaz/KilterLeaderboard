e2b97fbd4f12a08cbeaa88f7aff4e1ded5631f70
oelyaz <oelyaz.seytl@gmail.com>
2025-04-27 20:58:21 +0200

    added leaderboard api request and rm old dir src

--- front/src/lib/Leaderboard.svelte
+++ front/src/lib/Leaderboard.svelte
@@ -2,26 +2,30 @@
     import { onMount } from "svelte";
 
     type Climber = {
+        id: number;
+        kilterId: string;
+        username: string;
         name: string;
-        ascents: number;
         score: number;
+        grade: number;
     };
 
     let leaderboard: Climber[] = [];
 
     async function fetchLeaderboard() {
         try {
-            const res = await fetch("http://localhost:8080/api/leaderboard");
+            const res = await fetch("http://localhost:8080/users/");
             if (!res.ok) throw new Error("Failed to fetch Leaderboard!");
             leaderboard = await res.json();
         } catch (error) {
             console.error(error);
-            leaderboard = [
-                { name: "Test Player 1", ascents: 12, score: 999 },
-                { name: "Test Player 2", ascents: 2, score: 850 },
-                { name: "Test Player 3", ascents: 1400, score: 720 }
-            ];
         }
+        leaderboard = [
+            { "id": 1, "kilterId": "134", "username": "blub", "name": "Test Player 1", "score": 999, "grade": 12 },
+            { "id": 2, "kilterId": "134", "username": "blub", "name": "Test Player 2", "score": 20, "grade": 2 },
+            { "id": 3, "kilterId": "134", "username": "blub", "name": "Test Player 3", "score": 3000, "grade": 22 }
+        ];
+        leaderboard.sort((a: Climber, b: Climber) => b.score - a.score);
     }
 
     onMount(fetchLeaderboard);
@@ -32,7 +36,7 @@
         <tr>
             <th>Rank</th>
             <th>Name</th>
-            <th>Ascents</th>
+            <th>Grade</th>
             <th>Score</th>
         </tr>
     </thead>
@@ -41,7 +45,7 @@
             <tr>
                 <td>{index+1}</td>
                 <td>{player.name}</td>
-                <td>{player.ascents}</td>
+                <td>{player.grade}</td>
                 <td>{player.score}</td>
             </tr>
         {/each}
