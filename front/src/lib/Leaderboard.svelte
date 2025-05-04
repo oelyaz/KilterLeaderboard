5f0d9bc00037b77e5d8523bec99057cdf64e097e
Rebecca Prechtl <rlpmuc2006@gmail.com>
2025-05-04 18:16:03 +0200

    display grade in fb scale

--- front/src/lib/Leaderboard.svelte
+++ front/src/lib/Leaderboard.svelte
@@ -23,6 +23,36 @@
         console.log(leaderboard);
         leaderboard.sort((a: Climber, b: Climber) => b.score - a.score);
     }
+    function difficultyInFbScala(difficulty) {
+        const mapping = {
+            10: '4a',
+            11: '4b',
+            12: '4c',
+            13: '5a',
+            14: '5b',
+            15: '5c',
+            16: '6a',
+            17: '6a+',
+            18: '6b',
+            19: '6b+',
+            20: '6c',
+            21: '6c+',
+            22: '7a',
+            23: '7a+',
+            24: '7b',
+            25: '7b+',
+            26: '7c',
+            27: '7c+',
+            28: '8a',
+            29: '8a+',
+            30: '8b',
+            31: '8b+',
+            32: '8c',
+            33: '8c+'
+        };
+
+        return mapping[difficulty] || 'not a valid difficulty';
+    }
 
     onMount(fetchLeaderboard);
 </script>
@@ -41,7 +71,7 @@
             <tr>
                 <td>{index+1}</td>
                 <td>{player.username}</td>
-                <td>{player.grade}</td>
+                <td>{difficultyInFbScala(player.grade)}</td>
                 <td>{player.score}</td>
             </tr>
         {/each}
