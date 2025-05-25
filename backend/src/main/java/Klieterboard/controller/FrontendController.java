9e0c7c16e6d148d845271865e16a89b9afa14540
oelyaz <oelyaz.seytl@gmail.com>
2025-05-25 19:59:40 +0200

    fix cors with wildcard °_°

--- backend/src/main/java/Klieterboard/controller/FrontendController.java
+++ backend/src/main/java/Klieterboard/controller/FrontendController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
-@CrossOrigin(origins = "https://kilterleaderboard.de")
+@CrossOrigin(origins = "*")
 @Controller
 @RestController
 @RequestMapping(path = "/")
